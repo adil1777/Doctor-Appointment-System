56b6339814164a88f6364ae7a12b1f2e68e2c9f9
Mohd Adil <adilazmi760@gmail.com>
2024-02-06 11:51:47 +0530

    update .env

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,11 @@ const color = require('colors');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL); await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            retryWrites: true, // Ensure this option is included
+        });
         console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
 
     }catch(error){

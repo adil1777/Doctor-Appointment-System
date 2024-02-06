e7cae1a16071b73008140654a0b40895642047fe
Mohd Adil <adilazmi760@gmail.com>
2024-02-06 11:14:32 +0530

    update .env

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const color = require('colors');
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.MONGEDB_URL);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
 
     }catch(error){

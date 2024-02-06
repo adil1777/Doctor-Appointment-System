bf229d394542d5beda4e3cf6a2a9db2a6084e9a7
Mohd Adil <adilazmi760@gmail.com>
2024-02-06 15:32:25 +0530

    update db.js file

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-const color = require('colors');
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URL);
-        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
-
-    }catch(error){
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            retryWrites: true, // Ensure this option is included
+        });
+        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white);
+    } catch (error) {
         console.log(`MongoDb Server Issue ${error}`.bgRed.white);
     }
 };
-module.exports=connectDB;
+
+module.exports = connectDB;
